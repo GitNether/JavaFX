b4f4ccbb92f7e81b7c32d4c06736f9c3c89cffcc
Nether <trextim@gmx.de>
2022-05-13 10:22:27 +0200

    initial

---
+++ src/main/java/com/tl.prog2.exercises/set09/TestApplication.java
@@ -0,0 +1,28 @@
+package main.java.com.tl.prog2.exercises.set09;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+
+public class TestApplication extends Application {
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        createScene(primaryStage);
+        primaryStage.show();
+    }
+
+    protected void createScene(Stage primaryStage) {
+
+    }
+}
